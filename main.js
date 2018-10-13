fac4f65fe405abbeb2554efd2dfa02610e3cabe8
Eugenie Ordonneau <eugenie@ordonneau.com>
2018-10-13 16:14:53 +0100

    one-operator calculations are kind of working

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 const screen = document.querySelector(".screen");
 const buttons = Array.from(document.querySelectorAll("button"));
 var displayed = "";
+var i = 0;
+var nbs = [""];
+var operators = [];
 
 
 function add (a,b) {
@@ -58,22 +61,55 @@ function factorial(a) {
 
 
 function operate (nb1, operator, nb2){
-    if(operator == "plus"){
+    if(operator == "+"){
         return add(nb1,nb2);
-    } else if (operator == "minus"){
+    } else if (operator == "-"){
         return substract(nb1,nb2);
-    } else if (operator == "times"){
+    } else if (operator == "x"){
         return multiply(nb1,nb2);
-    } else if (operator == "divided"){
+    } else if (operator == "/"){
         return divide(nb1, nb2);
     } else {
         return "ERROR"
     }
 }
 
+
 buttons.forEach((button) => {
 	button.addEventListener("click", function(e){
+	if(nbs == [""]){
+		displayed="";
+	}
+	if(e.target.value!== "clear" && e.target.value!== "equals"){
 		displayed += e.target.value;
-		screen.textContent = displayed;
+		var str = e.target.value;//storing the string so i can use the function charCodeAt
+		if(str.charCodeAt(0)>47 && str.charCodeAt(0)<58){
+			nbs[i] += e.target.value;	//creating a string that's going to be the input number
+		} else {
+			operators[i]="";
+			operators[i] += e.target.value;
+			++i; //so next time a number is clicked on a new string is created and stored in the array nbs
+			nbs[i] = "";	
+		}
+
+			
+	}else{
+		if(e.target.value == "equals"){
+			while(i>=0){
+				nbs[i]= parseInt(nbs[i]);
+				i--;
+			}
+			displayed = operate(nbs[0],operators[0],nbs[1]);
+		} else {	
+			displayed = "";
+		}
+		nbs = [""];
+		operators = [];
+		i = 0;
+	}
+	console.log(operators);
+	screen.textContent = displayed;
+		
+		
 	});
 });
